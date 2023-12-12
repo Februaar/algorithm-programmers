552c235264a48c6c79cb8c0d6f202f68cf810ddc
Kyungjin Jung <kyoooungjin@gmail.com>
2023-12-12 21:10:26 +0900

    17

---
+++ 프로그래머스 입문/짝수의 합.js
@@ -0,0 +1,26 @@
+// 정수 n이 주어질 때,
+// n 이하의 짝수를 모두 더한 값 return
+
+function solution(n) {
+  var answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+      // i가 짝수인 경우에만 더함
+      answer += i;
+    }
+  }
+
+  return answer;
+}
+
+// 좀 더 간결하게
+function solution(n) {
+  var answer = 0;
+
+  for (let i = 2; i <= n; i += 2) {
+    answer += i;
+  }
+
+  return answer;
+}
